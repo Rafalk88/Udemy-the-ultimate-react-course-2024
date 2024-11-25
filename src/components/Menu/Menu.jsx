b4cbe242e36d2befd06de32d826ac21844cdffde
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-11-25 19:57:35 +0100

    feat(p1): add p to Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,6 +5,7 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious</p>
       {pizzaData.length === 0 ? (
         <p>No pizzas available at the moment.</p>
       ) : (
