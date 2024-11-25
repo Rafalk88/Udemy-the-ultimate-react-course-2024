adb3931ffad5d2327f302d01573c071519375efb
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-11-25 17:41:44 +0100

    feat(p1): add basic component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Pizza } from './components';
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Pizza />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Pizza/Pizza.jsx
@@ -0,0 +1,9 @@
+import { pizzaData } from '../../data';
+
+function Pizza() {
+  return (
+    <img src="pizzas/spinaci.jpg" alt="spinaci" />
+  )
+}
+
+export { Pizza };
