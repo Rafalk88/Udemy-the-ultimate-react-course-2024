360003e64d88271c32503c953065c85a87cd71f0
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-11-25 20:06:18 +0100

    feat(p1): add soldOut ternary operators to Pizza

--- src/components/Menu/Pizza/Pizza.jsx
+++ src/components/Menu/Pizza/Pizza.jsx
@@ -2,12 +2,12 @@ function Pizza({
   name, ingredients, price, photoName, soldOut
 }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${soldOut ? 'sold-out': ''}`}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>{price}$</span>
+        <span>{soldOut ? ('sold out').toUpperCase() : `${price}$`}</span>
       </div>
     </li>
   )
