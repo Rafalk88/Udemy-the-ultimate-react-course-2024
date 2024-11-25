056e944a8e7cd3b88b791f9ab9d3de0a7c518e90
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-11-25 19:51:11 +0100

    feat(p1): if operator if not list in Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,13 +5,18 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {
-          pizzaData.map((pizza) => (
-            <Pizza key={pizza.id} {...pizza} />
-          ))
-        }
-      </ul>
+      {pizzaData.length === 0 ? (
+        <p>No pizzas available at the moment.</p>
+      ) : (
+        <ul className="pizzas">
+          {
+            pizzaData.map((pizza) => (
+              <Pizza key={pizza.id} {...pizza} />
+            ))
+          }
+        </ul>
+      )
+      }
     </main>
   )
 }
